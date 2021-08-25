2d7432ce7be5d854a6cd975629eb52d0fc307460
apexcel <dev.apexcel@gmail.com>
2021-08-25 17:14:02 +0900

    Add hook

---
+++ src/hooks/useEmphasizer.ts
@@ -0,0 +1,9 @@
+import { useEffect } from "react";
+
+export default function useEmphasizer() {
+    const currentY = globalThis.scrollY;
+    const headers = document.querySelectorAll(`article > h2, h3, h4`)
+    useEffect(() => {
+
+    }, [])
+}
